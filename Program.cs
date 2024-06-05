5a634ff2adcd296062da05ae6cd0bec1cf00d554
jeremy woo <jeremy>
2024-06-06 05:28:13 +1200

    testing and understanding csharp12.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// alias features 
+
+using myInt32 = int;
+// ok for nullable value type
+using myNullableInt = int?;
+
+// ok to alias string 
+using myString = string;
+// not ok to use nullable reference type
+// using myNullableString = string?;
+
+myInt32 salary = 10000;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"My salary: {salary}!");
+
+// optional lambda expression 
+var CalculateTax = (int totalIncome, int taxRate = 1) => totalIncome * taxRate;
+var CalculateTax2 = (int totalIncome, float taxRate = 0.4f) => totalIncome * taxRate;
+
+Console.WriteLine($"Tax : {CalculateTax(5000)}!");
+
+// primary constructor
+class MyCar(int engine, int passengers)
+{
+    public int EngineCapability { get; set; } = engine;
+    public int NumberPassengers { get; set; } = passengers;
+}
+
